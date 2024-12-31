8aeda131c6bc0f2118e58571f6c96ad8e12949b6
87 <dev@87flowers.com>
2024-12-31 21:43:30 +0000

    [0.52] nmr more
    Elo   | 10.37 +- 7.95 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.98 (-2.94, 2.94) [0.00, 10.00]
    Games | N: 3886 W: 1102 L: 986 D: 1798
    Penta | [122, 450, 715, 502, 154]
    https://pyronomy.pythonanywhere.com/test/760/
    
    bench: 2584409

--- src/search.zig
+++ src/search.zig
@@ -112,23 +112,25 @@ fn search(game: *Game, ctrl: anytype, pv: anytype, alpha: Score, beta: Score, pl
         return static_eval;
     }
 
-    // Null-move pruning
+    // Null-move reduction and pruning
     if (!is_in_check and (mode == .normal or mode == .nullmove) and depth > 2 and !game.prevMove().isNone()) {
         const old_state = game.moveNull();
-        const null_score = -try search2(game, ctrl, line.Null{}, -beta, -beta +| 1, ply + 1, depth - 4, .normal);
+        const nws_reduction = 4 + @divFloor(depth, 6);
+        const null_score = -try search2(game, ctrl, line.Null{}, -beta, -beta +| 1, ply + 1, depth - nws_reduction, .normal);
         game.unmoveNull(old_state);
         if (null_score >= beta) {
             if (mode == .nullmove) {
-                // Failed high twice, prune
+                // Failed high twice, actually prune
                 pv.writeEmpty();
                 // Do not return mate scores
                 return if (eval.isMateScore(null_score)) beta else null_score;
             }
-            // Subtree verification search
+            // Null-move reduction
             // This is the same as a normal search except:
             // - With a "pruneable" flag set (the .nullmove mode)
             // - Depth reduced by 1
-            return search(game, ctrl, line.Null{}, alpha, beta, ply, depth - 1, .nullmove);
+            const nmr_reduction = 1 + @divFloor(depth, 6);
+            return search(game, ctrl, line.Null{}, alpha, beta, ply, depth - nmr_reduction, .nullmove);
         }
     }
 
